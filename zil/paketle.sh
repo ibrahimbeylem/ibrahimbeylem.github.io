e2373e277fb238ffe47bedf1d92de738531c3e6d
“validaliy“ <validaliy63@gmail.com>
2019-09-20 20:58:12 +0300

    “zil-betik”

---
+++ zil/paketle.sh
@@ -0,0 +1,22 @@
+for dosya in *.m4r
+do
+    if [ -f "$dosya" ]
+    then
+         echo  ${dosya%.m4r} klasörü oluşturuluyor 
+         mkdir ${dosya%.m4r}
+         echo DEBİAN Klasörü oluşturuluyor
+         mkdir ${dosya%.m4r}/DEBIAN
+         echo Library Klasörü oluşturuluyor
+         mkdir ${dosya%.m4r}/Library
+         echo Ringtones Klasörü oluşturuluyor
+         mkdir ${dosya%.m4r}/Library/Ringtones
+         echo Zil Sesi taşınıyor
+         cp $dosya ${dosya%.m4r}/Library/Ringtones/com.beylem.zilsesleri-$dosya
+         echo control dosyası kopyalanıyor
+         cp control ${dosya%.m4r}/DEBIAN/control
+         sed -i 's/xxxxxx/'${dosya%.m4r}' Zil Sesi/' ${dosya%.m4r}/DEBIAN/control
+         boyut=$(du $dosya)
+         boyut=$(echo $boyut | sed 's/.'$dosya'//')
+         sed -i 's/boyut/'$boyut'/' ${dosya%.m4r}/DEBIAN/control
+    fi
+done
